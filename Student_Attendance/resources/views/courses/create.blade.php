9821775650bd6f361e08b39ec88f9643685edbab
ALBaraa ALSaqile <albaraawork3@gmail.com>
2024-11-19 09:52:31 +0200

    course views (index,create)

--- Student_Attendance/resources/views/courses/create.blade.php
+++ Student_Attendance/resources/views/courses/create.blade.php
@@ -1,4 +1,44 @@
-<div>
+
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<div class="container">
     <!-- Happiness is not something readymade. It comes from your own actions. - Dalai Lama -->
     <h1>Create new Course</h1>
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+
+    <form method="post" action="{{ route('courses.store') }}">
+        @csrf
+
+        <div class="form-group">
+            <label for="course_id">Course id:</label>
+            <input
+                type="text"
+                id="course_id"
+                name="course_id"
+                class="form-control"
+                value="{{ $errors->any() ? old('course_id') : '' }}">
+        </div>
+
+        <div class="form-group">
+            <label for="name">Course Name:</label>
+            <input type="text" id="name" name="name" class="form-control" value="{{ $errors->any() ? old('name') : '' }}">
+        </div>
+
+        <div class="form-group">
+            <label for="description">Description:</label>
+            <input type="text" id="description" name="description" class="form-control" value="{{ $errors->any() ? old('description') : '' }}">
+        </div>
+
+        <button type="submit" class="btn btn-success">Add Course</button>
+    </form>
 </div>
+
--- Student_Attendance/resources/views/courses/index.blade.php
+++ Student_Attendance/resources/views/courses/index.blade.php
@@ -1,4 +1,36 @@
-<div>
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<div class="container">
+
+    <h1>Welcom to Courses page</h1>
+
+    @if (session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
+    @endif
+
+    <a href="{{ route('courses.create') }}" class="btn btn-primary mb-3">Add new Course</a>
     <!-- Simplicity is the consequence of refined emotions. - Jean D'Alembert -->
-    <h1>index.php</h1>
+    <table class = "table table-striped">
+        <thead>
+        <tr>
+            <th>Id</th>
+            <th>Coures id</th>
+            <th>Course Name</th>
+            <th>Description</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($courses as $course)
+        <tr>
+            <td>{{ $course->id }}</td>
+            <td>{{ $course->course_id }}</td>
+            <td>{{ $course->name }}</td>
+            <td>{{ $course->description }}</td>
+        </tr>
+        @endforeach
+        </tbody>
+    </table>
 </div>
