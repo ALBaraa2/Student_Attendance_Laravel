837b483bf0b1d3fc2651168820a2f6ad0e71af42
ALBaraa ALSaqile <albaraawork3@gmail.com>
2024-11-22 10:53:18 +0200

    add student id in student table

---
+++ Student_Attendance/resources/views/students/index.blade.php
@@ -0,0 +1,14 @@
+
+<div class="container">
+    <!-- Order your soul. Reduce your wants. - Augustine -->
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Student ID</th>
+                <th>Name</th>
+                <th></th>
+            </tr>
+        </thead>
+    </table>
+
+</div>
