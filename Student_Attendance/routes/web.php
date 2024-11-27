b37ef56af884d20b601d00d63c1d5d7aaf8ab661
ALBaraa ALSaqile <albaraawork3@gmail.com>
2024-11-27 11:11:14 +0200

    authintication

--- Student_Attendance/routes/web.php
+++ Student_Attendance/routes/web.php
@@ -1,18 +1,30 @@
 <?php
 
 use App\Http\Controllers\CourseController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SectionController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TeachAssistantController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('courses',CourseController::class)->except('edit','update');
-Route::resource('students', StudentController::class);
-Route::resource('assistants', TeachAssistantController::class);
-Route::resource('sections', SectionController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::resource('courses',CourseController::class)->except('edit','update');
+    Route::resource('students', StudentController::class);
+    Route::resource('assistants', TeachAssistantController::class);
+    Route::resource('sections', SectionController::class);
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
 
